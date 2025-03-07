954c1c3356d048c6f7bebb62639d9f464c4fb4a1
Yerkebulan777 <erkebulan777@gmail.com>
2025-03-07 22:58:47 +0500

    55555

--- LintelMaster/LintelManager.cs
+++ LintelMaster/LintelManager.cs
@@ -5,37 +5,29 @@ namespace LintelMaster;
 /// <summary>
 /// Основной класс для маркировки перемычек
 /// </summary>
-public partial class LintelManager(GroupingConfig config)
+public sealed class LintelManager(GroupingConfig config)
 {
     private readonly string _thickParam = config.ThickParameter;
     private readonly string _widthParam = config.WidthParameter;
     private readonly string _heightParam = config.HeightParameter;
 
     /// <summary>
-    /// Находит все семейства по наименованию
-    /// </summary>
-    /// <returns>Список перемычек</returns>
-    public List<FamilyInstance> GetInstancesByName(Document doc, string familyName)
-    {
-        BuiltInCategory bic = BuiltInCategory.OST_StructuralFraming;
-        return CollectorHelper.GetInstancesByFamilyName(doc, bic, familyName);
-    }
-
-    /// <summary>
     /// Категоризирует перемычки по их размерам
     /// </summary>
-    public Dictionary<SizeKey, List<LintelData>> CategorizeLintelData(List<FamilyInstance> lintels)
+    public Dictionary<SizeKey, List<LintelData>> RetrieveLintelData(Document doc, string familyName)
     {
         Dictionary<SizeKey, List<LintelData>> result = new(100);
 
-        foreach (FamilyInstance lintel in lintels)
+        BuiltInCategory bic = BuiltInCategory.OST_StructuralFraming;
+
+        foreach (FamilyInstance lintel in CollectorHelper.GetInstancesByFamilyName(doc, bic, familyName))
         {
             int thickRoundMm = Convert.ToInt32(UnitManager.FootToRoundedMm(LintelUtils.GetParamValue(lintel, _thickParam)));
             int widthRoundMm = Convert.ToInt32(UnitManager.FootToRoundedMm(LintelUtils.GetParamValue(lintel, _widthParam), 50));
             int heightRoundMm = Convert.ToInt32(UnitManager.FootToRoundedMm(LintelUtils.GetParamValue(lintel, _heightParam), 100));
 
             LintelData lintelData = new(lintel, thickRoundMm, widthRoundMm, heightRoundMm);
-  
+
             if (!result.TryGetValue(lintelData.GroupKey, out List<LintelData> group))
             {
                 result[lintelData.GroupKey] = group;
@@ -47,5 +39,7 @@ public partial class LintelManager(GroupingConfig config)
 
         return result;
     }
+
+
 
 }
---
+++ RevitBoost/Commands/LintelTaggingCommand.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using LintelMaster;
+using Nice3point.Revit.Toolkit.External;
+
+namespace RevitBoost.Commands
+{
+    /// <summary>
+    /// Команда для маркировки перемычек
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    public class LintelTaggingCommand : ExternalCommand
+    {
+        public override void Execute()
+        {
+            Document doc = Document;
+
+            LintelManager manager = new(new GroupingConfig());
+
+            Dictionary<SizeKey, List<LintelData>> lintels = manager.RetrieveLintelData(doc, "Перемычка");
+
+            _ = TaskDialog.Show("УРА!", $"Успешно промаркировано {lintels.Count} типов перемычек.");
+        }
+    }
+
+
+
+}
