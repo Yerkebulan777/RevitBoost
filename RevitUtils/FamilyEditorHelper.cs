897e796568c6b8b686cd5467bd7a625343cb2717
Yerkebulan <42465235+Yerkebulan777@users.noreply.github.com>
2025-03-11 20:59:57 +0500

    Util

---
+++ RevitUtils/FamilyEditorHelper.cs
@@ -0,0 +1,40 @@
+﻿namespace RevitUtils;
+
+/// <summary>
+/// Статический класс для работы с вложенными семействами в редакторе семейств
+/// </summary>
+public static class FamilyEditorHelper
+{
+    /// <summary>
+    /// Находит родительское семейство по экземпляру вложенного семейства
+    /// </summary>
+    public static FamilyInstance GetParentFamily(Document doc, FamilyInstance nestedInstance)
+    {
+        Element parent = nestedInstance.SuperComponent;
+
+        return parent is FamilyInstance instance ? instance : throw new Exception("Family does not have a super component!");
+    }
+
+    /// <summary>
+    /// Получает все вложенные семейства из семейства
+    /// </summary>
+    public static List<FamilyInstance> GetNestedFamilies(Document doc, FamilyInstance parent)
+    {
+        List<FamilyInstance> nestedInstances = [];
+
+        foreach (ElementId id in parent.GetSubComponentIds())
+        {
+            Element nestedElement = doc.GetElement(id);
+
+            if (nestedElement is FamilyInstance nested)
+            {
+                nestedInstances.Add(nested);
+            }
+        }
+
+        return nestedInstances;
+    }
+
+
+
+}
