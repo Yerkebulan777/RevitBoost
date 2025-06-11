2c6247d574e263d81d5f08b0ad96ca182ea17b65
Yerkebulan <42465235+Yerkebulan777@users.noreply.github.com>
2025-06-11 14:22:56 +0500

    Refract

--- RevitBoost/Application.cs
+++ RevitBoost/Application.cs
@@ -1,7 +1,9 @@
 ﻿using Autodesk.Revit.UI;
 using Nice3point.Revit.Toolkit.External;
 using RevitBoost.Commands;
+using System.IO;
 using System.Windows.Media.Imaging;
+
 
 namespace RevitBoost
 {
@@ -26,9 +28,9 @@ namespace RevitBoost
         {
             RibbonPanel panel = Application.CreatePanel("Commands", "RevitBoost");
             PushButton button = panel.AddPushButton<LintelLabelingCommand>("Execute");
-
-            Uri smallImageUri = new Uri("pack://application:,,,/RevitBoost;component/Resources/Icons/RibbonIcon16.png");
-            Uri largeImageUri = new Uri("pack://application:,,,/RevitBoost;component/Resources/Icons/RibbonIcon32.png");
+            string basePath = Path.Combine(Directory.GetCurrentDirectory(), "Resources", "Icons");
+            Uri smallImageUri = new Uri(Path.Combine(basePath, "RibbonIcon16.png"), UriKind.RelativeOrAbsolute);
+            Uri largeImageUri = new Uri(Path.Combine(basePath, "RibbonIcon32.png"), UriKind.RelativeOrAbsolute);
 
             button.Image = new BitmapImage(smallImageUri);
             button.LargeImage = new BitmapImage(largeImageUri);
