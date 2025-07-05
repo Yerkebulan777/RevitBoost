e1dc96734b4786326f00ff39db4c74b30f4e1c6f
Yerkebulan <42465235+Yerkebulan777@users.noreply.github.com>
2025-07-05 16:48:25 +0500

    ZZZ

---
+++ RevitBoost/Config/ServiceConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using CommonUtils;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using System.IO;
+
+namespace RevitBoost.Config
+{
+    public static class ServiceConfiguration
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services)
+        {
+            string logDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "RevitBoost");
+
+            services.AddLogging(builder =>
+            {
+                builder.ClearProviders();
+
+                Logger logger = new LoggerConfiguration()
+                    .MinimumLevel.Debug()
+                    .Enrich.WithProcessId()
+                    .Enrich.WithProcessName()
+                    .Enrich.WithEnvironmentName()
+                    .Enrich.WithProperty("UserName", Environment.UserName)
+                    .MinimumLevel.Override("System", LogEventLevel.Warning)
+                    .MinimumLevel.Override("Microsoft.Extensions", LogEventLevel.Warning)
+                    .WriteTo.Debug(restrictedToMinimumLevel: LogEventLevel.Debug)
+                    .WriteTo.File(
+                        path: Path.Combine(logDirectory, "revit-boost-.log"),
+                        rollingInterval: RollingInterval.Day,
+                        retainedFileCountLimit: 7)
+                    .CreateLogger();
+
+                builder.AddSerilog(logger, dispose: true);
+            });
+
+            services.AddSingleton<IModuleLoggerFactory, ModuleLoggerFactory>();
+
+            return services;
+        }
+    }
+
+}
--- RevitBoost/Host.cs
+++ RevitBoost/Host.cs
@@ -1,82 +1,28 @@
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Serilog;
-using Serilog.Events;
-using System.IO;
+using RevitBoost.Config;
 
 namespace RevitBoost
 {
     public static class Host
     {
-        private static IServiceProvider _services;
+        private static ServiceProvider _serviceProvider;
 
         public static void Start()
         {
-            if (_services != null)
-            {
-                return;
-            }
+            ServiceCollection services = new();
+            services.ConfigureServices();
 
-            ConfigureSerilog();
-
-            ServiceCollection serviceCollection = new();
-
-            _ = serviceCollection.AddLogging(config =>
-            {
-                _ = config.ClearProviders();
-                _ = config.SetMinimumLevel(LogLevel.Debug);
-                _ = config.AddFilter("Microsoft.Extensions", LogLevel.Warning);
-                _ = config.AddFilter("System", LogLevel.Warning);
-
-                // Добавляем Serilog как провайдер
-                _ = config.AddSerilog();
-            });
-
-            _services = serviceCollection.BuildServiceProvider();
-        }
-
-        private static void ConfigureSerilog()
-        {
-            // Создаем директорию для логов
-            string logDirectory = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                "RevitBoost", "logs");
-
-            _ = Directory.CreateDirectory(logDirectory);
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft.Extensions", LogEventLevel.Warning)
-                .MinimumLevel.Override("System", LogEventLevel.Warning)
-                .Enrich.WithProperty("MachineName", Environment.MachineName)
-                .Enrich.WithProperty("UserName", Environment.UserName)
-                .Enrich.WithProperty("Application", "RevitBoost")
-                .Enrich.FromLogContext()
-                .WriteTo.Debug()
-                .WriteTo.File(
-                    path: Path.Combine(logDirectory, "revit-boost-.log"),
-                    rollingInterval: RollingInterval.Day,
-                    retainedFileCountLimit: 7,
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] [Revit {RevitVersion}] [{MachineName}] {Message:lj}{NewLine}{Exception}")
-                .CreateLogger();
+            _serviceProvider = services.BuildServiceProvider();
         }
 
         public static T GetService<T>()
         {
-            return _services != null ? _services.GetService<T>() : default;
+            return _serviceProvider.GetService<T>();
         }
 
         public static void Stop()
         {
-            if (_services is IDisposable disposable)
-            {
-                disposable.Dispose();
-            }
-
-            Log.CloseAndFlush();
-            _services = null;
+            _serviceProvider?.Dispose();
         }
-
-
     }
 }
