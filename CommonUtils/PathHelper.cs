0c56e0d5a74fd06d0e4b50d058df5aeb7113e3c1
Yerkebulan <42465235+Yerkebulan777@users.noreply.github.com>
2025-07-09 16:51:57 +0500

    1234567

--- CommonUtils/PathHelper.cs
+++ CommonUtils/PathHelper.cs
@@ -1,22 +1,19 @@
-﻿using System.Diagnostics;
-
-namespace CommonUtils
+﻿namespace CommonUtils
 {
     public static class PathHelper
     {
         // Найти папку PROJECT
-        public static string FindProjectRoot(string currentPath)
+        public static string FindProjectRoot(string path)
         {
-            string parentDir = Directory.GetParent(currentPath).FullName;
-            string projectFolder = parentDir.FindFolder("*PROJECT*");
-            Debug.Assert(!string.IsNullOrEmpty(projectFolder));
-            return projectFolder;
+            DirectoryInfo dirInfo = Directory.GetParent(path);
+            string projectPath = dirInfo.FindFolder("*PROJECT*");
+            return projectPath is null ? dirInfo.FullName : projectPath;
         }
 
         // Найти папку по маске
-        public static string FindFolder(this string path, string pattern)
+        public static string FindFolder(this DirectoryInfo dirInfo, string pattern)
         {
-            return Directory.GetDirectories(path, pattern).FirstOrDefault();
+            return Directory.GetDirectories(dirInfo.FullName, pattern).FirstOrDefault();
         }
 
         // Создание директории, если не существует
@@ -24,7 +21,7 @@ namespace CommonUtils
         {
             if (!string.IsNullOrEmpty(path) && !Directory.Exists(path))
             {
-                _ = Directory.CreateDirectory(path);
+                Directory.CreateDirectory(path);
             }
 
             return path;
