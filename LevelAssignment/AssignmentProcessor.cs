6ab69206480c1721aa5a4c544a5b98ee2bc45590
Yerkebulan <42465235+Yerkebulan777@users.noreply.github.com>
2025-07-16 14:37:04 +0500

    123

--- LevelAssignment/AssignmentProcessor.cs
+++ LevelAssignment/AssignmentProcessor.cs
@@ -57,7 +57,7 @@ namespace LevelAssignment
 
             _ = output.AppendLine($"Shared parameter: {LevelSharedParameter?.Name}");
             _ = output.AppendLine($"Number of floors: {FloorDataCollection?.Count}");
-            _ = output.AppendLine("Start main process:");
+            _ = output.AppendLine("Start process:");
 
             foreach (FloorData floor in FloorDataCollection)
             {
