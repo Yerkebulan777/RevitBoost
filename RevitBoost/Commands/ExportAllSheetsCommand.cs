cef83bec48e09bb2989c3011c73a23cc7140deb0
Yerkebulan <42465235+Yerkebulan777@users.noreply.github.com>
2025-07-17 15:13:38 +0500

    StopWatch

--- RevitBoost/Commands/ExportAllSheetsCommand.cs
+++ RevitBoost/Commands/ExportAllSheetsCommand.cs
@@ -3,6 +3,8 @@ using Autodesk.Revit.UI;
 using CommonUtils;
 using ExportPdfTool;
 using RevitUtils;
+using Serilog;
+using System.Diagnostics;
 using System.IO;
 
 namespace RevitBoost.Commands
@@ -16,21 +18,28 @@ namespace RevitBoost.Commands
             string outputPath = RevitPathHelper.GetExportDirectory(doc, "03_PDF", out string revitFilePath);
             string revitFileName = Path.GetFileNameWithoutExtension(revitFilePath);
 
+            Log.Information("Export PDF started: {FileName}", revitFileName);
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
             try
             {
                 PathHelper.EnsureDirectory(outputPath);
                 RevitPdfExporter exporter = new(doc, outputPath);
                 exporter.ExportAllSheets(revitFileName);
 
+                stopwatch.Stop();
+
+                Log.Information("Export PDF completed {ElapsedTime:F2} min", stopwatch.Elapsed.TotalMinutes);
+
                 return Result.Succeeded;
             }
             catch (Exception ex)
             {
+                stopwatch.Stop();
+                Log.Error("Export PDF failed: {ErrorMessage}", ex.Message);
                 message = $"Error during export: {ex.Message}";
                 return Result.Failed;
             }
         }
-
-
     }
 }
